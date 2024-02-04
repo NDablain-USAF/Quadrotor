56e5c6d8eb84ef57564b9dd3c438f7ae7aaa6949
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-02-04 14:27:00 -0500

    Fixed filtering to not lose precision when coordinates in radians

--- src_F/GPS.ino
+++ src_F/GPS.ino
@@ -16,14 +16,14 @@ byte setOutput[51] = {36,80,77,84,75,51,49,52,44,48,44,48,44,48,44,49,44,48,44,4
 
 
 void setup() {
-  Serial.begin(9600);
-  /* Run this section on cold start
+  Serial.begin(115200);
+  ///Run this section on cold start
   Serial1.begin(9600); 
   Serial1.write(setBaud115200,20);
   delay(1000);
   Serial1.end();
   delay(1000);
-  */
+  ////////////////////////////////
   Serial1.begin(115200);
   delay(1000);
   Serial1.write(setUpdate10,25);
@@ -50,6 +50,13 @@ void loop() {
       uint8_t altitudeSize = sizeof(altitudeArray)/sizeof(altitudeArray[0]);
       double altitude = convertAlt(altitudeArray,altitudeSize);
       displacements(latitude,longitude,altitude);
+      /*
+      for (int i=altitudeSize-1;i>=0;i--){
+        Serial.print((char)altitudeArray[i]);
+      }
+      Serial.print(" , ");
+      */
+      //Serial.println(latitude*1e8);
     }
   }
   else {
@@ -59,63 +66,56 @@ void loop() {
 
 void displacements(double latitude, double longitude, double altitude){
   static uint32_t biasControl;
-  static uint32_t timelast;
-  static double xE, xN, h, Latitudelast, Longitudelast, Altitudelast, xNlast, xElast, hlast;
-  double c[4] = {0.9,0.995,0.10,0.005}; // Filter constants
+  static double xE, xN, h, LatitudeLast, LongitudeLast, AltitudeLast, xNLast, xELast, hLast;
+  double c[2] = {0.995,0.005}; // Filter constants
+  double precision = 1e-9; // Expand 
+  double precisionAlt = 1e-7;
   double r1 = 6378137; // in meters
   double r2 = 6356752;
-  double cutoff = (0.01)*(PI/180);
-  double Latitude = latitude*c[2] + Latitudelast*c[0];
-  double Longitude = longitude*c[3] + Longitudelast*c[1];
-  double Altitude = altitude*c[2] + Altitudelast*c[0];
-
-  if (biasControl==50000){
+  double Latitude = (latitude*(1/precision))*c[1] + LatitudeLast*c[0];
+  double Longitude = (longitude*(1/precision))*c[1] + LongitudeLast*c[0];
+  double Altitude = (altitude*(1/precisionAlt))*c[1] + AltitudeLast*c[0];
+  if (biasControl==300000){
     ++biasControl;
   }
-  else if (biasControl<50000){
+  else if (biasControl<300000){
     ++biasControl;
-    if ((biasControl==20000)||(biasControl==35000)){
+    if ((biasControl==1000)||(biasControl==150000)){
       Serial.println("Calibrating");
     }
   }
   else {
-    if ((abs(Latitude-Latitudelast)>cutoff)||(Latitude==0)||(Latitude==(PI/2))){
-      Latitude = Latitudelast*c[1];
-    }
-    if (abs(Longitude-Longitudelast)>cutoff){
-      Longitude = Longitudelast*c[1];
-    }
-    double radiusEarth = sqrt((pow(pow(r1,2)*cos(Latitude),2)+pow(pow(r2,2)*sin(Latitude),2))/(pow(r1*cos(Latitude),2)+pow(r2*sin(Latitude),2)));
-    double R = radiusEarth+Altitude;
-    xE += (R*(Longitude-Longitudelast)*sin(Latitude));
-    xN += (R*(Latitude-Latitudelast));
-    h += (Altitude-Altitudelast);
+    double radiusEarth = sqrt((pow(pow(r1,2)*cos(Latitude*precision),2)+pow(pow(r2,2)*sin(Latitude*precision),2))/(pow(r1*cos(Latitude*precision),2)+pow(r2*sin(Latitude*precision),2)));
+    double R = radiusEarth+(Altitude*precisionAlt);
+    xE += (R*((Longitude-LongitudeLast)*precision)*sin(Latitude*precision));
+    xN += (R*(Latitude-LatitudeLast)*precision);
+    h += ((Altitude-AltitudeLast)*precisionAlt);
 
     if (isnan(xE)==1){
-      xE = xElast;
+      xE = xELast;
     }
     if (isnan(xN)==1){
-      xN = xNlast;
+      xN = xNLast;
     }
     if (isnan(h)==1){
-      h = hlast;
+      h = hLast;
     }
-    
+
     Serial.print("Displacment North: ");
     Serial.print(xN);
     Serial.print(" , ");
     Serial.print("Displacement East: ");
     Serial.print(xE);
     Serial.print(" , ");
-    Serial.print("Height: ");
+    Serial.print("Altitude: ");
     Serial.println(h);
   }
-  xNlast = xN;
-  xElast = xE;
-  hlast = h;
-  Latitudelast = Latitude;
-  Longitudelast = Longitude;
-  Altitudelast = Altitude;
+  xNLast = xN;
+  xELast = xE;
+  hLast = h;
+  LatitudeLast = Latitude;
+  LongitudeLast = Longitude;
+  AltitudeLast = Altitude;
 }
 
 double convertGeo(uint8_t coordinate[],uint8_t coordinateSize,uint8_t indicator){ // Converts array in degrees and months to single value in radians
