a36dc29b46f03c914f9cabf069f57e8095891182
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-06-05 14:10:08 -0400

    Update Motor.h

--- src_O/Motor.h
+++ src_O/Motor.h
@@ -6,30 +6,30 @@
 class MOTOR {
   private:
     float
-      c1 = 0.95,
+      c1 = 0.95, // IIR constants for speed measurement
       c2 = 1-c1,
-      Y_int, // Integrated compensator output
-      Y_int2, // float integrated compensator output
-      X, // Compensator input
-      X_int, // Integrated compensator input
-      X_int2, // float integrated compensator input
-      p_lag = -0.1; // Pole of lag compensator, scaled by 10
+      Y_int, 
+      Y_int2, 
+      X, 
+      X_int, 
+      X_int2, 
+      p_lag = -0.1; // Pole of lag compensator
     uint32_t
-      time[2]; // time[0]: speed dt, time[1]: derivative control dt
+      time[2]; // time[0]: measurement dt, time[1]: compensator dt
     int16_t
       Y,
-      z_lead = -25, // -Zero of lead compensator (s domain)
+      z_lead = -25, // -Zero of lead compensator 
       z_lag = -2, // Zero of lag compensator
-      p_lead = -45, // Pole of lead compensator (s domain)
+      p_lead = -45, // Pole of lead compensator 
       e; // Difference between reference and measured speed values
     uint16_t
       w_measured,
       K = 5, // Gain of lead compensator
-      interval = 1e4; // How often to measure speed (pulses)
+      interval = 1e4; // How often to measure speed (microseconds)
     uint8_t
-      CPR = 17,
+      CPR = 17, // Counts per revolution, RS555EN 24V motor has 17 pulses per revolution
       pwm_pin,
-      input;
+      input; // Duty cycle of pwm
   public:
     MOTOR(uint8_t PWM_PIN);
     void 
