64e6bc7c2152efde78f5ebbc0f2414432e35f435
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-06-13 12:48:33 -0400

    Update Button.cpp

--- src_O/Button.cpp
+++ src_O/Button.cpp
@@ -17,11 +17,11 @@ void BUTTON::init(){
   for (uint8_t j=0;j<4;j++){
     pinMode(control_pins[j],OUTPUT);
   }
-  i = 0;
+  i = 0; // This will be used to toggle the motors on/off, 0:off, 0>:on
 }
 
 void BUTTON::receive(float EulerAngles[3]){
-  while (i < 1){
+  while (i < 1){ // This will keep the motors off until the on_pin is sent low via a mechanical button press
     delay(1);
     for (uint8_t j=0;j<4;j++){
       analogWrite(control_pins[j],0);
@@ -30,6 +30,11 @@ void BUTTON::receive(float EulerAngles[3]){
       ++i;
     }
   }
+  // The motors are disabled under the following conditions...
+  //        1: The off_pin is sent low via a mechanical button press
+  //        2: The roll or pitches angles exceed +-pi/12 radians (15 degrees)
+  //        3: The yaw angle exceeds +-pi/2 radians (90 degrees)
+  //        4: 30 seconds have passed since SYSTEM START (Not when the mechanical on_pin is pressed)
   if ((digitalRead(off_pin)==0)||(abs(EulerAngles[0])>(PI/12))||(abs(EulerAngles[1])>(PI/12))||(abs(EulerAngles[2])>PI/2)){
     i = 0;
   }
