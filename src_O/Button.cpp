aaa2077b60a4620a19fd79eadaa52b62f82c8699
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-07-08 15:05:07 -0400

    Update Button.cpp

--- src_O/Button.cpp
+++ src_O/Button.cpp
@@ -1,13 +1,13 @@
 #include "Button.h"
 
 BUTTON::BUTTON(const uint8_t POWER_PIN, const uint8_t ON_PIN, const uint8_t OFF_PIN, const uint8_t CONTROL_PINS[4]){
-    power_pin = POWER_PIN;
-    on_pin = ON_PIN;
-    off_pin = OFF_PIN;
-    for (uint8_t j=0;j<4;j++){
-      control_pins[j] = CONTROL_PINS[j];
-    }
-    init();
+  power_pin = POWER_PIN;
+  on_pin = ON_PIN;
+  off_pin = OFF_PIN;
+  for (uint8_t j=0;j<4;j++){
+    control_pins[j] = CONTROL_PINS[j];
+  }
+  init();
 }
 
 void BUTTON::init(){
@@ -20,7 +20,7 @@ void BUTTON::init(){
   i = 0; // This will be used to toggle the motors on/off, 0:off, 0>:on
 }
 
-void BUTTON::receive(float EulerAngles[3]){
+void BUTTON::receive(){
   while (i < 1){ // This will keep the motors off until the on_pin is sent low via a mechanical button press
     delay(1);
     for (uint8_t j=0;j<4;j++){
@@ -29,16 +29,5 @@ void BUTTON::receive(float EulerAngles[3]){
     if (digitalRead(on_pin)==0){
       ++i;
     }
-  }
-  // The motors are disabled under the following conditions...
-  //        1: The off_pin is sent low via a mechanical button press
-  //        2: The roll or pitches angles exceed +-pi/12 radians (15 degrees)
-  //        3: The yaw angle exceeds +-pi/2 radians (90 degrees)
-  //        4: 30 seconds have passed since SYSTEM START (Not when the mechanical on_pin is pressed)
-  if ((digitalRead(off_pin)==0)||(abs(EulerAngles[0])>(PI/12))||(abs(EulerAngles[1])>(PI/12))||(abs(EulerAngles[2])>PI/2)){
-    i = 0;
-  }
-  if (micros()>30e6){
-    i = 0;
   }
 }
