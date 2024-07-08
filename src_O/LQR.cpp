99b18b248e08b645f404058a502b9e4eaae62cb6
Nathan Dablain <145600105+NDablain-USAF@users.noreply.github.com>
2024-07-08 15:13:13 -0400

    Update LQR.cpp

--- src_O/LQR.cpp
+++ src_O/LQR.cpp
@@ -17,14 +17,14 @@ void LQR::control_attitude(int16_t r[3], int16_t MotorSpeeds[4], float EulerAngl
     if (abs(U[i+1])<2){
       e_int_attitude[i] += K_attitude[i][0]*e[i]*dt;
     }
-    // State regulation
+    // State regulation, interestingly a stationary LQR can be nearly approximated with a PID (P: Euler Angle, I: Integrated Euler Angle, D: Angular Rate)
+    // The only discrepancy being the reference frame difference of Euler angles and angular rates
     u[i] = e_int_attitude[i] - K_attitude[i][1]*y[i] - K_attitude[i][2]*w[i];
   }
   for (uint8_t i=1;i<4;i++){
     // Back to floating point numbers
     U[i] = u[i-1]*(PI/(180*1e5));
   }
-  //Serial.print(",");
   U[0] = Thrust;
   // Solve for corresponding motor speeds (squared motor speeds)
   omega[3] = U[0]/(4*K_f) - U[3]/(4*K_t) - U[2]/(2*K_f*l);
@@ -37,16 +37,19 @@ void LQR::control_attitude(int16_t r[3], int16_t MotorSpeeds[4], float EulerAngl
   }
 }
 
-void LQR::control_altitude(float h[2], float *r){
-  float e = *r-h[0];
+void LQR::control_altitude(float *height, float *r){
+  // Actually just a PID
+  *r -= decay_rate;
+  static float e_int;
   float dt = (micros()-timelast[1])/1e6;
   if (dt>0.1){
     dt = 0.1;
   }
   timelast[1] = micros();
+  float e = *r-*height;
   if ((Thrust<60)&&(Thrust>0)){
-    e_int_altitude += K_altitude[0]*e*dt;
+    e_int += e*dt;
   }
-  Thrust = e_int_altitude - K_altitude[1]*h[0] - K_altitude[2]*h[1];
-  //Serial.println(Thrust);
+  Thrust = K_altitude[0]*e_int + K_altitude[1]*e;
+  Thrust = constrain(Thrust,0,60);
 }
